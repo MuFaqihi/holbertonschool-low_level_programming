bdb1ea154353e4be2a493e0a955fecb3cb1e1d49
MuFaqihi <mmnouc2@gmail.com>
2025-02-20 13:07:09 +0000

    _putchar _putchar

---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>  /* Include the write library */
+
+/**
+ * _putchar - prints a single character
+ * @c: the character to be printed
+ *
+ * Return: 1 on success, -1 on error.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));  /* Use write to print the character */
+}
