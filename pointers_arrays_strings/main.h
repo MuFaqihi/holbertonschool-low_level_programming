d726a659d3bac4af21cde25747aacfca360ad5d5
MuFaqihi <mmnouc2@gmail.com>
2025-02-27 10:13:09 +0000

    strcpystrcpy

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,16 +1,8 @@
-#include <stddef.h>
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strcpy(char *dest, const char *src);
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
+
