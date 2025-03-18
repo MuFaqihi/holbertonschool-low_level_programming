8cf0cce1faf4810b6dd4a9bc8425ed86bd159c28
MuFaqihi <mmnouc2@gmail.com>
2025-03-18 10:57:56 +0000

    To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,40 +1,82 @@
-#include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - Prints anything based on the given format
- * @format: List of types of arguments passed to the function
+ * print_string - Prints a string
+ * @args: The argument list containing the string
+*/
+void print_string(va_list args)
+{
+char *string = va_arg(args, char *);
+if (string == NULL)
+string = "(nil)";
+printf("%s", string);
+}
+
+/**
+ * print_char - Prints a character
+ * @args: The argument list containing the character
+*/
+void print_char(va_list args)
+{
+printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Prints an integer
+ * @args: The argument list containing the integer
+*/
+void print_int(va_list args)
+{
+printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a floating-point number
+ * @args: The argument list containing the float
+*/
+void print_float(va_list args)
+{
+printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_all - Prints any type of argument based on format specifiers
+ * @format: A list of types of arguments passed
 */
 void print_all(const char * const format, ...)
 {
 va_list args;
-int i = 0;
-char *str, *separator = "";
+char *sep = "";
+int i = 0, j;
+
+print_form_t print_form[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string}
+};
 
 va_start(args, format);
 
 while (format && format[i])
 {
-switch (format[i])
+j = 0;
+while (j < 4)
 {
-case 'c':
-printf("%s%c", separator, va_arg(args, int));
-break;
-case 'i':
-printf("%s%d", separator, va_arg(args, int));
-break;
-case 'f':
-printf("%s%f", separator, va_arg(args, double));
-break;
-case 's':
-str = va_arg(args, char *);
-printf("%s%s", separator, str ? str : "(nil)");
+if (*print_form[j].c == format[i])
+{
+printf("%s", sep);
+print_form[j].f(args);
+sep = ", ";
 break;
 }
-separator = ", ";
+j++;
+}
 i++;
 }
 
-va_end(args);
 printf("\n");
+va_end(args);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,17 +2,24 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
+#include <stdio.h>
 
-typedef struct printer
+/**
+ * struct print_form - Struct for printing different types
+ * @c: Type specifier character
+ * @f: Function pointer to the appropriate print function
+ */
+typedef struct print_form
 {
-    char *symbol;
-    void (*print)(va_list arg);
-} printer_t;
+    char *c;
+    void (*f)(va_list args);
+} print_form_t;
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
+/* Function prototypes */
 void print_all(const char * const format, ...);
+void print_string(va_list args);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
 
-#endif
-
+#endif /* VARIADIC_FUNCTIONS_H */
